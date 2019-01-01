b604bf3755689244946d4abe1ad1fe4553e4ceed
Tim Tyrrell <tim@tyrrell.io>
2019-01-01 14:20:57 -0700

    Render textboxes based on the board type value

--- src/Board.js
+++ src/Board.js
@@ -4,7 +4,7 @@ class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      boardType: '4x4',
+      boardType: '4',
     }
 
     this.handleBoardTypeChange = this.handleBoardTypeChange.bind(this)
@@ -14,16 +14,26 @@ class Board extends Component {
     this.setState({ boardType: event.target.value })
   }
 
+  renderTextBoxes() {
+    const textBoxes = []
+    for (let x = 0; x < this.state.boardType; x++) {
+      for (let y = 0; y < this.state.boardType; y++) {
+        textBoxes.push(<input key={`${x}${y}`} type="text" id={`${x}${y}`} autoComplete="off" size="1" />)
+      }
+    }
+    return textBoxes
+  }
+
   render() {
     return (
       <div className="Board">
         <select value={this.state.boardType} onChange={this.handleBoardTypeChange}>
-          <option value="4x4">4x4</option>
-          <option value="5x5">5x5</option>
-          <option value="6x6">6x6</option>
+          <option value="4">4x4</option>
+          <option value="5">5x5</option>
+          <option value="6">6x6</option>
         </select>
 
-        render textboxes based on board state
+        { this.renderTextBoxes ()}
       </div>
     )
   }
--- src/Board.test.js
+++ src/Board.test.js
@@ -10,3 +10,19 @@ it('renders without crashing', () => {
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('renders correct number of textboxes by default', () => {
+  const checkbox = shallow(<Board />);
+  expect(checkbox.find('input').length).toEqual(16)
+});
+
+it('renders correct number of textboxes on selection change', () => {
+  const checkbox = shallow(<Board />);
+  expect(checkbox.find('input').length).toEqual(16)
+
+  checkbox.find('select').simulate('change', { target: { value: "5" } })
+  expect(checkbox.find('input').length).toEqual(25)
+
+  checkbox.find('select').simulate('change', { target: { value: "6" } })
+  expect(checkbox.find('input').length).toEqual(36)
+});
