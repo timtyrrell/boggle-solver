ff55ee08a327d5826fb6423d5f90fcfbaf3b1a2f
Tim Tyrrell <tim@tyrrell.io>
2019-01-01 15:41:36 -0700

    Silly semicolon cleanup

--- src/Board.test.js
+++ src/Board.test.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 import Board from './Board'
-
 
 it('renders without crashing', () => {
   const component = renderer.create(<Board />)
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 it('renders correct number of textboxes by default', () => {
-  const checkbox = shallow(<Board />);
+  const checkbox = shallow(<Board />)
   expect(checkbox.find('input').length).toEqual(16)
-});
+})
 
 it('renders correct number of textboxes on selection change', () => {
-  const checkbox = shallow(<Board />);
+  const checkbox = shallow(<Board />)
   expect(checkbox.find('input').length).toEqual(16)
 
   checkbox.find('select').simulate('change', { target: { value: "5" } })
@@ -25,4 +24,4 @@ it('renders correct number of textboxes on selection change', () => {
 
   checkbox.find('select').simulate('change', { target: { value: "6" } })
   expect(checkbox.find('input').length).toEqual(36)
-});
+})
