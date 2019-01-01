f52d49e8b1f8a74b243766d7f0677dbbe128d69a
Tim Tyrrell <tim@tyrrell.io>
2019-01-01 14:12:26 -0700

    Add board type selections

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 class Board extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      boardType: '4x4',
+    }
+
+    this.handleBoardTypeChange = this.handleBoardTypeChange.bind(this)
+  }
+
+  handleBoardTypeChange = event => {
+    this.setState({ boardType: event.target.value })
+  }
+
   render() {
     return (
       <div className="Board">
-        render checkbox and textboxes here
+        <select value={this.state.boardType} onChange={this.handleBoardTypeChange}>
+          <option value="4x4">4x4</option>
+          <option value="5x5">5x5</option>
+          <option value="6x6">6x6</option>
+        </select>
+
+        render textboxes based on board state
       </div>
     )
   }
