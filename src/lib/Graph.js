21333e768e872b3a3f4dd1db299c0f12ad74407b
Tim Tyrrell <tim@tyrrell.io>
2019-01-01 19:38:19 -0700

    First pass at graph walking algorithm... not working correctly

--- src/lib/Graph.js
+++ src/lib/Graph.js
@@ -1,10 +1,12 @@
 import Node from './Node'
 
 class Graph {
-  constructor(boardSize, boggleLetters) {
+  constructor(boardSize, boggleLetters, dictionary = []) {
     this.boardSize = Number(boardSize)
     this.boggleLetters = boggleLetters
     this.nodes =  this.createNodes(this.boardSize, boggleLetters)
+    this.dictionary = dictionary
+    this.matchedWords = []
   }
 
   createNodes(boardSize, boggleLetters) {
@@ -44,25 +46,48 @@ class Graph {
         }
 
         if (columnOffsetPosition >= 0 && columnOffsetPosition <= boardSize && boggleLetters[rowPosition][columnOffsetPosition]) {
-          if (!connectedNodes.includes(`${rowPosition},${columnOffsetPosition}`)) {
-            connectedNodes.push(`${rowPosition},${columnOffsetPosition}`)
+          if (!connectedNodes.includes(`${rowPosition}${columnOffsetPosition}`)) {
+            connectedNodes.push(`${rowPosition}${columnOffsetPosition}`)
           }
         }
 
         if (rowOffsetPosition >= 0 && rowOffsetPosition <= boardSize && boggleLetters[rowOffsetPosition][columnPosition]) {
-          if (!connectedNodes.includes(`${rowOffsetPosition},${columnPosition}`)) {
-            connectedNodes.push(`${rowOffsetPosition},${columnPosition}`)
+          if (!connectedNodes.includes(`${rowOffsetPosition}${columnPosition}`)) {
+            connectedNodes.push(`${rowOffsetPosition}${columnPosition}`)
           }
         }
 
         if(columnOffsetPosition >= 0 && columnOffsetPosition <= boardSize && rowOffsetPosition >= 0 && rowOffsetPosition <= boardSize && boggleLetters[rowOffsetPosition][columnOffsetPosition]) {
-          if (!connectedNodes.includes(`${rowOffsetPosition},${columnOffsetPosition}`)) {
-            connectedNodes.push(`${rowOffsetPosition},${columnOffsetPosition}`)
+          if (!connectedNodes.includes(`${rowOffsetPosition}${columnOffsetPosition}`)) {
+            connectedNodes.push(`${rowOffsetPosition}${columnOffsetPosition}`)
           }
         }
       })
     })
-    return connectedNodes
+    return connectedNodes.sort()
+  }
+
+  findWordMatches(pathLetters = '', nodePosition = '00', visited = []) {
+    if (visited.includes(nodePosition)) return
+    visited.push(nodePosition)
+
+    const row = Number(nodePosition.slice(0,1))
+    const column = Number(nodePosition.slice(1,2))
+    const node = this.nodes.find((node) => node.rowPosition === row && node.columnPosition === column)
+    pathLetters += node.value
+
+    // need length logic for 5x5 and 6x6
+    if (pathLetters.length > 3) {
+      const word = this.dictionary.find((word) => word === pathLetters)
+      if (word) this.matchedWords.push(word)
+    }
+
+    node.connectedNodes.forEach((child) => {
+      const row = Number(child.slice(0,1))
+      const column = Number(child.slice(1,2))
+      const foundChild = this.nodes.find((node) => node.rowPosition === row && node.columnPosition === column)
+      this.findWordMatches(pathLetters, `${foundChild.rowPosition}${foundChild.columnPosition}`, visited)
+    })
   }
 }
 
--- src/lib/Graph.test.js
+++ src/lib/Graph.test.js
@@ -3,11 +3,13 @@ import Graph from './Graph'
 
 const boardSize = '4'
 const formattedLetters = [
-  ['a','p','p','x'],
-  ['x','x','l','x'],
+  ['a','p','p','l'],
+  ['e','x','e','e'],
   ['n','x','e','x'],
   ['x','x','x','Z']
 ]
+
+const dictionary = ['aa', 'apple']
 
 it('verifies defaults', () => {
   const graph = new Graph(boardSize, formattedLetters)
@@ -23,10 +25,16 @@ it('generates a correct connected node list', () => {
   const graph = new Graph(boardSize, formattedLetters)
   // [0,0]
   expect(graph.nodes[0].connectedNodes.length).toEqual(3)
-  expect(graph.nodes[0].connectedNodes).toEqual(['0,1', '1,0', '1,1'])
+  expect(graph.nodes[0].connectedNodes).toEqual(['01', '10', '11'])
   // [1,1]
   expect(graph.nodes[5].connectedNodes.length).toEqual(8)
   expect(graph.nodes[5].connectedNodes).toEqual(
-    ['1,2', '2,1', '2,2', '1,0', '2,0', '0,1', '0,2', '0,0']
+    ['00', '01', '02', '10', '12', '20', '21', '22']
   )
+})
+
+it('walks the graph to find matches', () => {
+  const graph = new Graph(boardSize, formattedLetters, dictionary)
+  graph.findWordMatches()
+  expect(graph.matchedWords).toEqual(['apple'])
 })
